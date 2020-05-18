ae8463258139a0d106171a5a93fe6712220dd645
popofrank97 <popofrank97@gmail.com>
2020-05-18 11:36:59 -0400

    added library

---
+++ Hardware Validation/accel_test/accel_test.ino
@@ -0,0 +1,16 @@
+/*
+ * This is a test that checks both accelerometers
+ */
+
+#include <Wire.h> // I2C library
+#include <SparkFunLSM9DS1.h>  // SparkFun LSM9DS1 library
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

213b69b2cc71dae57736593d86b4d3d587a1ac22
popofrank97 <popofrank97@gmail.com>
2020-05-18 12:48:26 -0400

    finished analog code

---
+++ Hardware Validation/analog_test/analog_test.ino
@@ -0,0 +1,36 @@
+/*
+ * This is a test to check all analog inputs
+ */
+
+#define analog_1 36
+#define analog_2 39
+#define analog_3 34
+#define analog_4 35
+#define analog_5 32
+#define analog_6 33
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(analog_1, INPUT);
+  pinMode(analog_2, INPUT);
+  pinMode(analog_3, INPUT);
+  pinMode(analog_4, INPUT);
+  pinMode(analog_5, INPUT);
+  pinMode(analog_6, INPUT);
+}
+
+int val_1, val_2, val_3, val_4, val_5, val_6;
+char out[256];
+void loop() {
+  val_1 = analogRead(analog_1);
+  val_2 = analogRead(analog_2);
+  val_3 = analogRead(analog_3);
+  val_4 = analogRead(analog_4);
+  val_5 = analogRead(analog_5);
+  val_6 = analogRead(analog_6);
+
+  sprintf(out, "%d, %d, %d, %d, %d, %d\n", val_1, val_2, val_3, val_4, val_5, val_6);
+  Serial.print(out);
+  delay(200);
+}
